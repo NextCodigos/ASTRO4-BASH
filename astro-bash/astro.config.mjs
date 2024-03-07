7aa58e465941d0affcbb8b0ab0bb3411a8a4812c
NextCodigos <nebulanemesis372@gmail.com>
2024-03-07 18:05:04 +0100

    Añado en astro.config.mjs la forma de cargar el render con hybrid y en pagina about hago estatica

--- astro-bash/astro.config.mjs
+++ astro-bash/astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output:'hybrid',
+  // output:'server', // para subir informacion a diario menos rapido
+  // output:'static',//mas rapido pero no actualiza informacion base de datos
   integrations: [tailwind()]
 });
