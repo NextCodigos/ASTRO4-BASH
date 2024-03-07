97b244136a23d8afdfeaaab4528b6c6b5e26201b
NextCodigos <nebulanemesis372@gmail.com>
2024-03-07 18:09:24 +0100

    Añado en astro.config.mjs la forma de cargar el render con static y en pagina about hago que se ejecute en servidor

--- astro-bash/astro.config.mjs
+++ astro-bash/astro.config.mjs
@@ -4,8 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output:'hybrid',
+  // output:'hybrid',
   // output:'server', // para subir informacion a diario menos rapido
-  // output:'static',//mas rapido pero no actualiza informacion base de datos
+  output:'static',//mas rapido pero no actualiza informacion base de datos
   integrations: [tailwind()]
 });
